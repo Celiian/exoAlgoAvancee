6dd78b957fb2eba3197236101c3c90e26116a0c1
celiian <celian.opigez@gmail.com>
2022-01-24 10:09:13 +0100

    Code en brut et fonction de réduction de prix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int prixProduit = 10;
+
+        int prixProduitFinal = prixProduit;
+
+        prixProduitFinal = prixProduitFinal - (50 * prixProduitFinal /100);
+        prixProduitFinal = prixProduitFinal - (30 * prixProduitFinal /100);
+
+        System.out.println(prixProduitFinal);
+
+        int prixFinal = Reductionprix(30, Reductionprix(50, prixProduit));
+        System.out.println(prixFinal);
+    }
+
+
+    public static int Reductionprix(int pourcentage, int prix){
+        prix = prix - (pourcentage * prix /100);
+
+        return prix;
     }
 }
