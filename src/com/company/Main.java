e7909c0b7e04ab35fe1bc4dd57856340bf46ff6d
celiian <celian.opigez@gmail.com>
2022-01-24 10:31:13 +0100

    Fonction de réduction successives + correction du code en brut et de la fonction Reductionprix() (passage de int en float pour plus de précision)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,43 @@ public class Main {
     public static void main(String[] args) {
         int prixProduit = 10;
 
-        int prixProduitFinal = prixProduit;
+        float prixProduitFinal = prixProduit;
 
         prixProduitFinal = prixProduitFinal - (50 * prixProduitFinal /100);
         prixProduitFinal = prixProduitFinal - (30 * prixProduitFinal /100);
 
         System.out.println(prixProduitFinal);
 
-        int prixFinal = Reductionprix(30, Reductionprix(50, prixProduit));
+        float prixFinal = Reductionprix(30, Reductionprix(50, prixProduit));
         System.out.println(prixFinal);
+
+
+        int[] tabreduc = new int[2];
+        tabreduc[0] = 50;
+        tabreduc[1] = 30;
+
+        System.out.println(Reduc(10, tabreduc));
+
     }
 
 
-    public static int Reductionprix(int pourcentage, int prix){
+    public static float Reductionprix(int pourcentage, float prix){
         prix = prix - (pourcentage * prix /100);
-
         return prix;
+    }
+
+
+    public static String Reduc(int prixOriginal, int[] tabReduc){
+        float prix = prixOriginal;
+        for (int i = 1; i < tabReduc.length; i ++ ){
+                if (tabReduc[i] > tabReduc[i-1]){
+                     return  "ERREUR : Veuillez ranger vos prix par ordre décroissant";
+                }
+        }
+        for (int pourcentage:tabReduc) {
+            prix = prix - (pourcentage * prix / 100);
+        }
+
+        return "Prix avec réductions : " + prix;
     }
 }
